c4994b5e91b3dffd9a3a43fc736d4d9ec7a4eeaf
ZacksAryasatya <syafazaqi02@gmail.com>
2025-08-02 21:14:01 +0700

    first commit

---
+++ Assets/Coin.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    private Collider coinCollider;
+
+    public Vector3 spawnAreaMin = new Vector3(-5, 0.5f, -5);
+    public Vector3 spawnAreaMax = new Vector3(5, 0.5f, 5);
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        coinCollider = GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            meshRenderer.enabled = false;
+            coinCollider.enabled = false;
+
+            Invoke(nameof(Respawn), 5f);
+        }
+    }
+
+    void Respawn()
+    {
+
+        float randX = Random.Range(spawnAreaMin.x, spawnAreaMax.x);
+        float randY = spawnAreaMin.y; 
+        float randZ = Random.Range(spawnAreaMin.z, spawnAreaMax.z);
+
+        transform.position = new Vector3(randX, randY, randZ);
+
+        meshRenderer.enabled = true;
+        coinCollider.enabled = true;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 4f;
+    private Rigidbody rb;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
